e1c2c26edafc1ddb672146b09b80b863381ef46f
trisnapermadi7 <114981399+trisnapermadi7@users.noreply.github.com>
2023-11-22 10:30:29 +0800

    Add files via upload

---
+++ PRAKTIKUM4/src/Soal1/Main.java
@@ -0,0 +1,7 @@
+package Soal1;
+public class Main {
+    public static void main (String []Args) {
+        HewanPeliharaan Kucing1 = new HewanPeliharaan (null, null);
+        Kucing1.display();
+    }
+}
---
+++ PRAKTIKUM4/src/Soal2/HewanPeliharaan.java
@@ -0,0 +1,14 @@
+package Soal2;
+class HewanPeliharaan {
+    private String nama;
+    private String ras;
+    public HewanPeliharaan(String r, String n) {
+        this.ras = r;
+        this.nama = n;
+    }
+    public void display() {
+        System.out.println("Detail Hewan Peliharaan:");
+        System.out.println("Nama hewan peliharaanku adalah : " + nama);
+        System.out.println("Dengan ras : " + ras);
+    }
+}
